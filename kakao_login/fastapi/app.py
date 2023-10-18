71baeb861a2c5b3e59b8bdeeedc6a2e282c1b331
ah00ee <nhoahyoung@gmail.com>
2023-10-18 16:59:48 +0900

    feat: 토큰 발급 구현

--- kakao_login/fastapi/app.py
+++ kakao_login/fastapi/app.py
@@ -4,6 +4,8 @@ from fastapi.responses import RedirectResponse
 from dotenv import load_dotenv
 
 import os
+import json
+import requests
 
 app = FastAPI()
 
@@ -18,6 +20,22 @@ async def main():
     return RedirectResponse(url=url)
 
 @app.get('/oauth/kakao/login')
-async def get_token_info(code: str=""):
+async def get_token_info(code: str):
+    res = requests.post('https://kauth.kakao.com/oauth/token',
+                        headers={"Content-type": "application/x-www-form-urlencoded;charset=utf-8"},
+                        data=
+                        {
+                            "grant_type":"authorization_code",
+                            "client_id":CLIENT_ID,
+                            "redirect_uri":REDIRECT_URI,
+                            "code":code
+                        }).content.decode()
+
+    res = json.loads(res)
+
+    os.environ["ACCESS_TOKEN"] = res["access_token"]
+    os.environ["EXPIRES_IN"] = str(res["expires_in"])
+    os.environ["REFRESH_TOKEN"] = res["refresh_token"]
+    os.environ["REFRESH_TOKEN_EXPIRES_IN"] = str(res["refresh_token_expires_in"])
 
     return {"message": "Success"}
